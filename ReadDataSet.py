31bb0d49b61d294c410c8f756516cd0e6ba42678
zhangzhen <107239840+zhang-zhen-project@users.noreply.github.com>
2023-08-09 10:51:18 +0800

    Update ReadDataSet.py

--- ReadDataSet.py
+++ ReadDataSet.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 # Define Root catalogue of datasets
-FatherPath = ("D:\Code\FIRE\FIRE")
+FatherPath = ("D:\code\FIRE")
 DataPath = (FatherPath + "/DATA")
 
 
@@ -17,16 +17,7 @@ def MapLoc(key):
 # Get feature and label
 def ScanerFile(url, InitPreDf, InitPreLabelLoc, InitPreLabelDam, total, slide, list_c, t):
     file = os.listdir(url)
-    # 初始化一个DataFrame空表
-    for f in file:
-        if (f.find("70") != -1 or f.find("100") != -1 or f.find("130") != -1) and (f.find("M") != -1):
-            InitPreDf = pd.read_csv(DataPath + "/" + f, usecols=list_c
-                                    # range(1, 201)
-                                    , skiprows=2, nrows=1,
-                                    engine='python',
-                                    header=None)
-        InitPreDf = InitPreDf.drop(index=InitPreDf.index)
-        break
+    ArrayDFList = []
     for f in file:
         if f.find("70") != -1 or f.find("100") != -1 or f.find("130") != -1:
             print("Reading: " + f, end=' ')
@@ -39,7 +30,10 @@ def ScanerFile(url, InitPreDf, InitPreLabelLoc, InitPreLabelDam, total, slide, l
                              header=None)
 
             for i in range(total - slide):
-                InitPreDf = pd.concat([InitPreDf, df.loc[i:i + slide - 1, :]], axis=0)
+                a = np.array(df.loc[i:i + slide - 1, :])
+                ArrayDFList.append(a)
+
+                # InitPreDf = pd.concat([InitPreDf, df.loc[i:i + slide - 1, :]], axis=0)
                 InitPreLabelLoc.append(MapLoc(str(f[:-11])))
                 InitPreLabelDam.append(int(f[-11:-9]))
             # df["label"] = PreLabel
@@ -47,22 +41,32 @@ def ScanerFile(url, InitPreDf, InitPreLabelLoc, InitPreLabelDam, total, slide, l
             print("Down\n")
 
             # InitPreLabel = np.array(InitPreLabel)
-    InitArrayDf = np.array(InitPreDf)
-    # InitArrayDf = InitArrayDf.reshape(len(InitPreLabel), Slide, 200)
+    for num in ArrayDFList:
+        if num.shape[0] != slide:
+            print('FALSE')
+    InitArrayDf = np.stack(ArrayDFList)
+    # InitArrayDf = np.array(InitPreDf)
+    InitArrayDf = InitArrayDf.reshape(-1, len(list_c))
     InitArrayLabelLoc = np.array(InitPreLabelLoc)
     InitArrayLabelDam = np.array(InitPreLabelDam)
-    np.save('DATASET/Temperature/InitArrayDf' + t + '.npy', InitArrayDf)
-    np.save('DATASET/Temperature/InitArrayLabelLoc' + t + '.npy', InitArrayLabelLoc)
-    np.save('DATASET/Temperature/InitArrayLabelDam' + t + '.npy', InitArrayLabelDam)
-
+    np.save('./DATASET/InitArrayDf' + t + '-' + str(slide) + str(len(list_c)) + '.npy', InitArrayDf)
+    np.save('./DATASET/InitArrayLabelLoc' + t + '-' + str(slide) + str(len(list_c)) + '.npy',
+            InitArrayLabelLoc)
+    np.save('./DATASET/InitArrayLabelDam' + t + '-' + str(slide) + str(len(list_c)) + '.npy',
+            InitArrayLabelDam)
 
 # Define init dataSets
 InitPreDf = pd.DataFrame()
 InitPreLabelLoc = []
 InitPreLabelDam = []
-soot_sensors = list(range(1, 201))
-temp_sensors = list(range(406, 606))
-sensor = soot_sensors + temp_sensors
-sample_time = 8
 
-ScanerFile(DataPath, InitPreDf, InitPreLabelLoc, InitPreLabelDam, int(2 * sample_time), sample_time/2, sensor, str(sensor))
+# select sensor numbers
+soot_sensors = list([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+temp_sensors = list([213, 220, 230, 240, 250, 260, 270, 278, 280, 290])
+sensors = soot_sensors + temp_sensors
+# select duration of fire
+time = 300
+# set time windows
+slide = 30
+# generate dataset
+ScanerFile(DataPath, InitPreDf, InitPreLabelLoc, InitPreLabelDam, int(2 * time), slide, sensors, str(time))
